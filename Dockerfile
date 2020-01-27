b7f8e521fd4982a4a2df193cfe65079cf60ecccb
Joshua <psyjr4@nottingham.ac.uk>
2020-01-27 22:30:54 +0000

    Changed port mapping

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ FROM mcr.microsoft.com/dotnet/core/aspnet:2.1
 WORKDIR /app
 COPY --from=build-env /app/out .
 
-EXPOSE 8001
+EXPOSE 80
 
 ENTRYPOINT ["dotnet", "recipe-api.dll"]
--- restartRecipeApi.sh
+++ restartRecipeApi.sh
@@ -7,7 +7,7 @@ docker rm recipe-api
 echo "Build the new recipe-api docker image"
 docker build -t recipe-api-image .
 echo "Running the new recipe-api container"
-docker run --name recipe-api --restart always -p 35001:5001 -d recipe-api-image
+docker run --name recipe-api --restart always -p 35001:80 -d recipe-api-image
 echo ""
 
 echo "Complete"
