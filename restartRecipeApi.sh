8f0f7d91fcd661d4fe4c57f6fb3fe5b0d0aaca0c
Joshua <psyjr4@nottingham.ac.uk>
2020-01-27 22:11:37 +0000

    Added shell script for updating container

---
+++ restartRecipeApi.sh
@@ -0,0 +1,14 @@
+echo "Pulling changes"
+git pull
+echo "Stopping recipe-api container"
+docker stop recipe-api
+echo "Remove recipe-api container"
+docker rm recipe-api
+echo "Build the new recipe-api docker image"
+docker build -t recipe-api-image .
+echo "Running the new recipe-api container"
+docker run --name recipe-api --restart always -p 35001:5001 -d recipe-api-image
+echo ""
+
+echo "Complete"
+echo ""
