5a8ec26d5fc86ed48377b52064d5d5c9b4765d18
Joshua <psyjr4@nottingham.ac.uk>
2020-01-31 07:53:03 +0000

    Return the data in a nice format

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -41,14 +41,8 @@ public class RecipesController : ControllerBase
     public async Task<ActionResult<string>> Get(string id)
     {
         var recipe = await DatabaseConnection.Connection("SELECT * FROM recipes WHERE id='" + id + "';");
-		System.Console.WriteLine(">>>>" + recipe);
-		var details = JsonConvert.DeserializeObject<RecipeData>(recipe);
-
-		System.Console.WriteLine($"Data {details}");
-
-		var json = JsonConvert.SerializeObject(details);
-		System.Console.WriteLine(json);
-		return json;
+		
+		return recipe;
 	}
 }
 
